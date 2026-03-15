72c43155dbc93faf2af9f146616d5bb3228a1f6f
Dima Sydorenko <dima.sydorenko.38@gmail.com>
2026-03-15 16:50:15 +0200

    add tests

--- src/test/java/com/QoQTestProject/notes/NotesApplicationTests.java
+++ src/test/java/com/QoQTestProject/notes/NotesApplicationTests.java
@@ -15,7 +15,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.EmptySource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
